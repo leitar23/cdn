bdda141c13af10d0071e47e052f8ff4a39eb9fba
qxz23 <3119476850@qq.com>
2022-05-21 15:58:07 +0800

    '第4次提交'

--- js/runtime.js
+++ js/runtime.js
@@ -32,5 +32,5 @@ setInterval(() => {
     else{
       currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/我的小屋-休息了-6adea8?style=social&logo=eslgaming' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
     }
-    document.getElementById("workboard").innerHTML = currentTimeHtml;
+    document.getElementById("workboard").innerHTML = currentTimeHtml
   }, 1000);
